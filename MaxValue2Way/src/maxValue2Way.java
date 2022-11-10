0aa8ac22f2b8e9944971d7aaffef8352664a307a
TrongAnh16 <anhtrduong@gmail.com>
2022-11-10 22:51:30 +0700

    .

---
+++ MaxValue2Way/src/maxValue2Way.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxValue2Way {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a row: ");
+        int r = sc.nextInt();
+        System.out.print("Enter a column: ");
+        int c = sc.nextInt();
+
+        int[][] arr = new int[r][c];
+        System.out.println("Enter elements for matrix: ");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print("arr[" + i + "][" + j + "] = ");
+                arr[i][j] = sc.nextInt();
+            }
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                }
+            }
+        }
+        System.out.println("Max value in array is: "+max);
+    }
+}
