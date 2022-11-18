7eff4afd71547fe750e3c06862ec1669ed920248
TrongAnh16 <anhtrduong@gmail.com>
2022-11-18 13:47:49 +0700

    .

--- Resizeable/src/Main/Main.java
+++ Resizeable/src/Main/Main.java
@@ -6,18 +6,24 @@ import shape.Square;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Rectangle before resized: ");
         Rectangle rectangle = new Rectangle(12, 20);
         System.out.println(rectangle);
+        System.out.println("Rectangle after resized: ");
         Rectangle rectangleResized =  rectangle.resize(50 );
         System.out.println(rectangleResized);
 
+        System.out.println("Circle before resized: ");
         Circle circle = new Circle(50);
         System.out.println(circle);
+        System.out.println("Circle after resized: ");
         Circle circleResized = circle.resize(50);
         System.out.println(circleResized);
 
+        System.out.println("Square before resized: ");
         Square square = new Square(10);
         System.out.println(square);
+        System.out.println("Square after resized: ");
         Square squareResized = square.resize(50);
         System.out.println(squareResized);
     }
