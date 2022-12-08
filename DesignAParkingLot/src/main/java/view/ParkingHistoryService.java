98c7f51a18c87585a566b99aa5582a594a28dbae
TrongAnh16 <anhtrduong@gmail.com>
2022-12-08 09:08:21 +0700

    .

---
+++ DesignAParkingLot/src/main/java/view/ParkingHistoryService.java
@@ -0,0 +1,8 @@
+package view;
+
+public class ParkingHistoryService {
+    private final String PATH = "./data/parkingHistory.txt";
+    private static FileUtils fileUtils = FileUtils.getInstance();
+
+
+}
